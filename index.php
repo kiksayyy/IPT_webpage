f1966862fb074a207f2ab4d1eda7cc8091ef797a
MATININGJELEANE <matiningjeleane@gmail.com>
2023-04-22 15:37:50 +0800

    php login script added

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+<?php
+//session function
+SESSION_START();
+
+//create a pre defined username and password since we do not have databate
+$acc_username = "gandamokikay";
+$acc_password = "matining25";
+$acc_fullname = "Jeleane M. Matining";
+$acc_address = "Marinduque PH";
+//check the current url for the redirections later
+$url_add = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+
+
+//condition to know if the button is clicked
+if (isset($_REQUEST['Login_button']) ===true) {
+	//get the username and the password from the form and compare to the predefined username and password
+		//kapag mali agad ang username
+	if($_REQUEST['form_username'] != $acc_username) {
+			header("Location:".$url_add." ?notexist");
+		}
+		//tama ang username mali ang password
+		else if ($_REQUEST ['form_username'] == $acc_username && $_REQUEST['form_password'] != $acc_password) {
+			header("Location: ".$url_add."?wrongpass");
+			// code...
+		}
+		//tama ag username at password
+		elseif ($_REQUEST['form_username']== $acc_username && $_REQUEST['form_password'] == $acc_password) {
+			header("Location:".$url_add. "?success");
+			
+			// create a session variables
+			$_SESSION['ses_username'] =$acc_username;
+			$_SESSION['ses_password'] =$acc_password;
+			$_SESSION['ses_fullname'] =$acc_fullname;
+			$_SESSION['ses_address'] =$acc_address;
+		
+		}//end of correct username and password
+}
+
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -24,13 +63,41 @@
 		      	</div>
 		      	<h3 class="text-center mb-4">MyTumblr Login</h3>
 						
-						<form action="#" class="login-form">
+						<form method="POST" class="login-form">
+		      		
 		      		<div class="form-group">
-		      			<input type="text" class="form-control rounded-left" placeholder="Username" required>
+
+		      			<?php 
+		      			//messaging
+		      			if(isset($_REQUEST['notexist'])===true) { 
+		      				echo "<div class ='alert alert-danger' role ='alert'> Username does not exist...</div>";
+
+		      			} else if (isset($_REQUEST['wrongpass']) === true) {
+		      				echo "<div class ='alert alert-warning' role ='alert'> Incorrect Password...</div>";
+
+		      				// code...
+		      			} else if (isset($_REQUEST['success']) ===true){
+		      				echo "<div class ='alert alert-success' role ='alert'> Redirecting..</div>";
+		      				header("Refresh: 5; url=account.php");
+		      			} else if(isset($_REQUEST['logout'])===true){
+		      				echo "<div class ='alert alert-info' role ='alert'> Thank You...</div>";
+		      			} else if (isset($_REQUEST['logfirst'])===true) {
+		      				echo "<div class ='alert alert-info' role ='alert'> Please Login First...</div>";
+		      			} else if (isset($_SESSION['ses_username']) ===true) {
+		      				echo "<div class ='alert alert-warning' role ='alert'> Still Login. Please <a href= 'account.php'>click here </a> to proceed.</div>";
+
+		      			}
+		      			
+
+		      			?>
+
+		      			<input type="text" class="form-control rounded-left" placeholder="Username" name= "form_username"required>
 		      		</div>
+
 	            <div class="form-group d-flex">
-	              <input type="password" class="form-control rounded-left" placeholder="Password" required>
+	              <input type="password" class="form-control rounded-left" placeholder="Password" name="form_password"required>
 	            </div>
+
 	            <div class="form-group d-md-flex">
 	            	<div class="w-50">
 	            		<label class="checkbox-wrap checkbox-primary">Remember Me
@@ -41,11 +108,14 @@
 								<div class="w-50 text-md-right">
 									<a href="#">Forgot Password</a>
 								</div>
+
 	            </div>
 	            <div class="form-group">
-	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5">Get Started</button>
+	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5" name="Login_button">Get Started</button>
 	            </div>
 	          </form>
+
+
 	        </div>
 				</div>
 			</div>
